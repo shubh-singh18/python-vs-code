8b58706be548325baac4c52a5b4819bd7a00d7ce
shubh-singh18 <shubhsingh9335882148@gmail.com>
2026-07-26 20:14:39 +0530

    swap two number and string

--- swap two number.py
+++ swap two number.py
@@ -1,5 +1,42 @@
 #swap two number 
-a=20;
-b=40;
+a=100;
+b=200;
 a,b=b,a
 print(a,b)
+
+#swap two number with the help of c
+a=50
+b=100
+c=a
+a=b
+b=c
+print(a,b)
+
+#swap two number other method
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+#swap two string
+a="chauhan"
+b="shubh singh"
+a,b=b,a
+print(a,b)
+
+#print add two number
+a=20
+b=40
+print(a+b)
+
+#print divide two number
+a=50
+b=5
+print(a/b)
+
+#print multiple two number
+a=45
+b=12
+print(a*b)
