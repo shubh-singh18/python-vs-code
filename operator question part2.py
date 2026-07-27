23369fc9ecf8193bfd8b69730e78e6717d478667
shubh-singh18 <shubhsingh9335882148@gmail.com>
2026-07-27 17:36:40 +0530

    operator question part2.py

---
+++ operator question part2.py
@@ -0,0 +1,49 @@
+#Write a program to find the remainder when one number is divided by another.
+
+#a=int(input("enter a first number"))
+#b=int(input("enter a second nummber "))
+#print(a%b)
+
+#write a program to calculate the square and cube of a number 
+
+#a=int(input("enter a first number"))
+#square=a**2
+#cube=a**3
+#print(square)
+#print(cube)
+
+#write  a program to swap two number without using a thrid variable
+
+#a=20
+#b=50
+#a,b=b,a
+#print(a,b)
+
+# write a program to check whether two nummber are equal or not
+#a=40
+#b=40
+#print(a==b)
+
+#write a program to find the greater of two number
+#a=int(input("enter a first number"))
+#b=int(input("enter a second number"))
+#result=a if a>b else b
+#print(result)
+
+
+#write a program to check whether a person is eligible  for voting
+#age=int(input("enter a age"))
+#print("they can a vote"  if age>=18 else "they can not a vote")
+
+# write a program to check whether a number is positive , negative , or zero
+#num=int(input("enter a number"))
+#print("Number is positive",num>0)
+#print("Number is negative",num<0)
+#print("Number is zero",num==0)
+
+#write a program to find the largest of three number
+a=int(input("enter a first number"))
+b=int(input("enter a second nummber"))
+c=int(input("enter a thrid number"))
+largest= a if a>b and a>c else b if b>c else c
+print(largest)
