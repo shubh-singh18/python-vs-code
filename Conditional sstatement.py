832108e89922bf38176203011f7b878f8c8ab4e9
shubh-singh18 <shubhsingh9335882148@gmail.com>
2026-07-27 18:30:39 +0530

    conditional statement

---
+++ Conditional sstatement.py
@@ -0,0 +1,46 @@
+# statement are three type 
+# conditional statement-> if,else,elif
+#looping statement-> for,while
+#jump statement->break,contine,pass
+
+#conditional statement
+#conditional statement are used to make decisions based on whether a condition is true or false
+
+# if statment  
+
+# Exectes a block of code if the condition is true.
+
+#example
+
+age=20
+if age>=18:
+ print("you are an adult") 
+
+# if else statment
+
+#Exectes one block if the condition is true and another if it is false
+
+#example
+
+age=16
+if age>18:
+ print("you can vote")
+else:
+ print("you can not vote")
+
+# if elif else statment
+
+#used when there are multiple conditions
+
+#example
+
+marks=39
+if marks>=90:
+ print("grade a")
+elif marks>=75:
+ print("grade b")
+elif marks>=33:
+ print("grade c")
+else:
+ print("fail")
+
