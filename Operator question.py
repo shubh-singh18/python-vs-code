7561865ade7ee0057734374b6f4f462ce89b0438
shubh-singh18 <shubhsingh9335882148@gmail.com>
2026-07-27 09:27:29 +0530

    operator question part1.py

---
+++ Operator question.py
@@ -0,0 +1,52 @@
+#Average of three number
+a=float(input("enter a first number"))
+b=float(input("enter a second number"))
+c=float(input("enter a third number"))
+result=(a+b+c)/3
+print(result)
+
+#write a program python to print simple interest .
+principal=100000
+rate=12
+time=2
+interest=(principal*rate*time)/100
+print(interest)
+
+#find area of circumference.
+radius=float(input("enter a number"))
+circumference=(2*3.15*radius)
+print(circumference)
+
+#calculate time 
+a=float(input("enter the time in hours"))
+b=float(input("enter the time in minutes"))
+c=float(input("enter the time in second"))
+x=str("hours")
+y=str("minutes")
+z=str("second")
+e=a*60,(y)
+f=b*60,(z)
+g=a*60*60,(z)
+h=b/60,(x)
+i=c/(60*60),(x)
+j=c/60,(y)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+
+# write a program to input two numbers and perform all arithmetic operation
+a=int(input("enter a first number"))
+b=int(input("enter a second number"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+
+
+
+
